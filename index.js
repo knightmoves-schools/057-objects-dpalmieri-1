74c4cc2bf9b97bccf2d8dd49c6a9f943cf4198d7
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-17 18:08:56 +0000

    School description

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+var school = {
+    'name': 'Highschool',
+    'average class size': 200,
+    mascot: 'tiger',
+};
+var schoolDescription = `The school name is ${school.name}, the average class size is ${school['average class size']}, and the mascot is a ${school['mascot']}`;
+document.getElementById('result').innerHTML = schoolDescription
