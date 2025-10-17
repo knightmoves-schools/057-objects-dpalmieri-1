2d0d5954be4753b75f8db046196437fb27afccb8
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-17 18:15:13 +0000

    The code works

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var school = {
     mascot: 'tiger',
 };
 var schoolDescription = `The school name is ${school.name}, the average class size is ${school['average class size']}, and the mascot is a ${school['mascot']}`;
-document.getElementById('result').innerHTML = `The school name is ${school.name}, the average class size is ${school['average class size']}, and the mascot is a ${school['mascot']}`
+document.getElementById('result').innerHTML = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school['mascot']}`
