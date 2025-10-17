a6829f2fba8679196129f788d0087b35a6dc5be0
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-17 18:10:42 +0000

    Rework

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var school = {
     mascot: 'tiger',
 };
 var schoolDescription = `The school name is ${school.name}, the average class size is ${school['average class size']}, and the mascot is a ${school['mascot']}`;
-document.getElementById('result').innerHTML = schoolDescription
+document.getElementById('result').innerHTML = `The school name is ${school.name}, the average class size is ${school['average class size']}, and the mascot is a ${school['mascot']}`
